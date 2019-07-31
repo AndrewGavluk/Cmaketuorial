c1fa863a918e52da7d7ec4f37b5c345b2dc8e0e8
Gawluk <gavl.andr96@gmail.com>
2019-07-31 22:47:38 +0300

    Step3

---
+++ Step3/Step3.cpp
@@ -0,0 +1,33 @@
+﻿// Step1.cpp: определяет точку входа для приложения.
+// A simple program that computes the square root of a number
+#include <stdio.h>
+#include <iostream>
+#include <stdlib.h>
+#include <math.h>
+#include "Step3config.h"
+#ifdef USE_MYMATH
+#include "MathFunctions.h"
+#endif
+
+int main(int argc, char* argv[])
+{
+
+		fprintf(stdout, " Version %d.%d\n",
+			Tutorial_VERSION_MAJOR,
+			Tutorial_VERSION_MINOR);
+		fprintf(stdout, "Usage: %s number\n", argv[0]);
+
+	double inputValue;
+
+	std::cin >> inputValue;
+
+#ifdef USE_MYMATH
+	double outputValue = mysqrt(inputValue);
+#else
+	double outputValue = sqrt(inputValue);
+#endif
+
+	fprintf(stdout, "The square root of %g is %g\n",
+		inputValue, outputValue);
+	return 0;
+}
